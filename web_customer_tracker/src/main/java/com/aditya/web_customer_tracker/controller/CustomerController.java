9a7bad7a3b23c6e546c1d768992ea562ddb4d814
aditya <aditya@192.168.2.11>
2019-07-11 01:05:19 +0530

    Added redirection from index page.varified database connection.

--- web_customer_tracker/src/main/java/com/aditya/web_customer_tracker/controller/CustomerController.java
+++ web_customer_tracker/src/main/java/com/aditya/web_customer_tracker/controller/CustomerController.java
@@ -1,5 +1,15 @@
 package com.aditya.web_customer_tracker.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
+	@GetMapping("/showCustomerList")
+	public String showCustomerList() {
+		return "list-customer";
+	}
 }
